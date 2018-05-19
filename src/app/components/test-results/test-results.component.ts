060a06ff9fd11a5f07b66e3334d061086ab88425
Rustam <archer1strong@gmail.com>
2018-05-19 09:56:23 +0600

    test results buttons

--- src/app/components/test-results/test-results.component.ts
+++ src/app/components/test-results/test-results.component.ts
@@ -1,14 +1,22 @@
-import {Component, DoCheck, OnInit} from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-test-results',
   templateUrl: './test-results.component.html',
   styleUrls: ['./test-results.component.css']
 })
-export class TestResultsComponent implements OnInit, DoCheck {
+export class TestResultsComponent implements DoCheck {
   lang: number;
   thanks = ['Спасибо!', 'Рахмат!'];
   questionLabel = ['Отражающий вопрос:', 'Ойлуу суроо:'];
+  buttons = [
+    {
+      text: ['Пройти другой тест', 'Дагы бир Тест алып'],
+    }
+    // text: ['Поделиться через фейсбук', 'Фейсбукта бөлүшүү'],
+    // text: ['Поделиться на Twitter', 'Twitter бөлүшүү'],
+  ];
   info = [
     {
       title: ['Откровенность', 'Ачыктык'],
@@ -26,13 +34,11 @@ export class TestResultsComponent implements OnInit, DoCheck {
       ]
     }
   ];
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(
+    private router: Router
+  ) { }
 
   ngDoCheck() {
     this.lang = Number(localStorage.lang);
   }
-
 }
